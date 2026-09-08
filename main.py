d57e093dc512b254a6be378c6e3a99e0df3be8a0
SyedMoosa110 <syedmoosa110@gmail.com>
2026-09-08 13:08:52 +0500

    Configure Vercel serverless deployment and WSGI entrypoint

--- main.py
+++ main.py
@@ -230,9 +230,9 @@ def create_app():
     return app
 
 
-if __name__ == '__main__':
-    app = create_app()
+app = create_app()
 
+if __name__ == '__main__':
     host = os.getenv('HOST', '0.0.0.0')
     port = int(os.getenv('PORT', '5000'))
 
